964139e2ec8d9fa38c0d3b4f7b5a8fac027bc58a
nikita <nik.k200181@gmail.com>
2020-05-19 13:30:40 +0400

    some editing

--- src/Client.java
+++ src/Client.java
@@ -1,22 +1,37 @@
 import java.io.*;
-import java.net.ConnectException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class Client {
+
+    static Socket socket;
+    static BufferedReader bufferedReader;
+    static BufferedReader bufferedReader1;
+    static OutputStream outputStream;
+    static InputStream inputStream;
+    static PrintWriter printWriter;
+
     public static void main(String[] args) throws IOException{
-            Socket socket = new Socket();
+            socket = new Socket();
             socket.connect(new InetSocketAddress(InetAddress.getLocalHost(),5757), 2000);
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            OutputStream outputStream = socket.getOutputStream();
-            PrintWriter printWriter = new PrintWriter(outputStream,true);
-            Scanner scanner1 = new Scanner(System.in);
+
+            outputStream = socket.getOutputStream();
+            printWriter = new PrintWriter(outputStream,true);
+
+            inputStream = socket.getInputStream();
+            bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            System.out.println("Server is ready to work...");
+
             System.out.print("Write INTEGER to calculate Fibonachi: ");
-            String inform = scanner1.nextLine();
-            printWriter.println(inform);
-            String result = bufferedReader.readLine();
-            System.out.println(result);
+
+            bufferedReader1 = new BufferedReader(new InputStreamReader(System.in));
+            String s = bufferedReader1.readLine();
+            printWriter.println(s);
+
+            String calcResult = bufferedReader.readLine();
+            System.out.println(calcResult);
+
     }
 }
